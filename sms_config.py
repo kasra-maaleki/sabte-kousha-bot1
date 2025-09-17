d0dc04ca69558e5aba7cd3d1b43674cda9267680
kasra-maaleki <kasra.maaleki@gmail.com>
2025-09-18 01:11:31 +0330

    Create sms_config.py

---
+++ sms_config.py
@@ -0,0 +1,7 @@
+import os
+
+
+NIAZ_USER: str = os.getenv("NIAZ_USER", "s.09128687292")
+NIAZ_PASS: str = os.getenv("NIAZ_PASS", "sxh@023")
+NIAZ_SENDER: str = os.getenv("NIAZ_SENDER", "500020021")
+NIAZ_BASE: str = os.getenv("NIAZ_BASE", "http://login.niazpardaz.ir/api/v1/RestWebApi/")
